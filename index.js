b2582506246c6a9466317f7d6a366bc7c1de2eec
Rezki-Pratama <rezki.pratama7777@gmail.com>
2020-11-27 10:04:08 +0700

    emit and broadcast

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
 const express = require('express')
+const socket = require('socket.io')
 
-//App setup
+//Setup
 const app = express()
 const server = app.listen(4000, ()=>{
     console.log('Listen to request on port 4000')
 })
 
+//Static
 app.use(express.static('public'))
+
+//Socket
+const io = socket(server)
+
+io.on('connection', (socket)=>{
+    
+    console.log('made socket connection', socket.id)
+
+    // Handle chat event
+    socket.on('chat', function(data){
+        // console.log(data);
+        io.sockets.emit('chat', data);
+    });
+
+    // Handle typing event
+    socket.on('typing', function(data){
+        socket.broadcast.emit('typing', data);
+    });
+})
